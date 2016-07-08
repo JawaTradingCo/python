1089644e962cde968e49d64493608925bab5078e
Red Dwarf <red@debian>
2016-07-08 13:43:31 -0500

    Added raw_input

--- age.py
+++ age.py
@@ -1,20 +1,22 @@
 import datetime
 import sys, getopt
-import string
+import re
 
 # usage: age.py yyyy-mm-dd
 # example: age.py 1972-05-07
 
-def calculate_age(argv):
-    for arg in argv:
-        d = arg.split("-",2)
-
-    today = datetime.date.today()
-   
- 
-    born = datetime.date(int(d[0]),int(d[1]),int(d[2]))
-    return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
-
+def calculate_age(age):
+	d = age.split("-",2)
+	if re.findall(r"\b([0-9]{4})(-)([0-9]{2})(-)([0-9]{2})\b",age):
+		today = datetime.date.today()
+		born = datetime.date(int(d[0]),int(d[1]),int(d[2]))
+		age =  today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+		print str(age) + " years old"
+	else:
+		print "Improper format"
+		return
+		
 if __name__ == "__main__":
-   age = calculate_age(sys.argv[1:])
-   print(age)
+	age = raw_input("Enter a date [yyyy-mm-dd]: ")
+	age = calculate_age(age)
+	
